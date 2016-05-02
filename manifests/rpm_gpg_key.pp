fcd3254ac1fed59d15bfc36cfb8fc353a5e2df0e
Okumura Takahiro <hfm.garden@gmail.com>
2016-05-03 00:26:50 +0900

    Add percona::repo (#1)
    * Add percona::repo
    
    * Report coverage
    
    * Add repo test
    
    * Add spec_helper for acceptance testing
    
    * Fix
    
    * Fix os arch name

---
+++ manifests/rpm_gpg_key.pp
@@ -0,0 +1,35 @@
+# Class: percona::rpm_gpg_key
+# ===========================
+#
+# Import the RPM GPG key for the Percona release.
+#
+# Parameters
+# ----------
+#
+# * `ensure`
+# Whether the RPM-GPG-KEY-Percona file should exist.
+#
+# * `path`
+# The path to the RPM-GPG-KEY-percona file to manage. Must be an absolute path.
+#
+class percona::rpm_gpg_key (
+  $ensure = present,
+  $path   = '/etc/pki/rpm-gpg/RPM-GPG-KEY-Percona',
+){
+
+  file { $path:
+    ensure  => $ensure,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => file('percona/RPM-GPG-KEY-Percona'),
+    before  => Exec['import-percona'],
+  }
+
+  exec { 'import-percona':
+    command => "rpm --import ${path}",
+    path    => ['/bin', '/usr/bin'],
+    unless  => "rpm -q gpg-pubkey-$(gpg --throw-keyids ${path} | grep pub | cut -c 12-19 | tr '[A-Z]' '[a-z]')",
+  }
+
+}
---
+++ spec/acceptance/repo_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper_acceptance'
+
+describe 'percona class' do
+  let(:manifest) {
+    <<-EOS
+      include ::percona::repo
+    EOS
+  }
+
+  it "should work without errors" do
+    result = apply_manifest(manifest, :acceptable_exit_codes => [0, 2], :catch_failures => true)
+
+    expect(result.exit_code).not_to eq 4
+    expect(result.exit_code).not_to eq 6
+  end
+
+  it "should run a second time without changes" do
+    result = apply_manifest(manifest)
+    expect(result.exit_code).to eq 0
+  end
+
+  context 'RedHat', :if => os[:family] == 'redhat' do
+    describe file('/etc/pki/rpm-gpg/RPM-GPG-KEY-Percona') do
+      it { should be_file }
+    end
+
+    %W(
+      percona-release-#{os[:arch]}
+      percona-release-noarch
+    ).each do |repo|
+      describe yumrepo(repo) do
+        it { should be_enabled }
+      end
+    end
+
+    %W(
+      percona-release-source
+      percona-testing-#{os[:arch]}
+      percona-testing-noarch
+      percona-testing-source
+      percona-experimental-#{os[:arch]}
+      percona-experimental-noarch
+      percona-experimental-source
+    ).each do |repo|
+      describe yumrepo(repo) do
+        it { should exist }
+        it { should_not be_enabled }
+      end
+    end
+  end
+end
