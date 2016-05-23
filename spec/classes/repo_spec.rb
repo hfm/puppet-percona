9203b4500f4695ae1c5d3b64bf58537a9e58b7c9
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-05-24 02:19:25 +0900

    Add unit tests for repo

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 describe 'percona::repo' do
 
   context 'with default values for all parameters' do
+    it { should compile }
+    it { should compile.with_all_deps }
     it { should contain_class('percona::repo') }
   end
 end
