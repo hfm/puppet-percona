eafb7a01c20fbb24eb7d04593c9740ee4f3eceaa
Okumura Takahiro <hfm.garden@gmail.com>
2016-05-25 11:47:46 +0900

    Add enabled attr to percona::repo (#6)

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,14 +2,26 @@
 # ===========================
 #
 # percona::repo is to setup repository.
-class percona::repo {
+class percona::repo (
+  $ensure                                = present,
+
+  $percona_release_basearch_enabled      = 1,
+  $percona_release_noarch_enabled        = 1,
+  $percona_release_source_enabled        = 0,
+  $percona_testing_basearch_enabled      = 0,
+  $percona_testing_noarch_enabled        = 0,
+  $percona_testing_source_enabled        = 0,
+  $percona_experimental_basearch_enabled = 0,
+  $percona_experimental_noarch_enabled   = 0,
+  $percona_experimental_source_enabled   = 0,
+) {
 
   case $::osfamily {
     'RedHat': {
       require percona::rpm_gpg_key
 
       Yumrepo {
-        ensure   => present,
+        ensure   => $ensure,
         gpgcheck => 1,
         gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Percona',
       }
@@ -18,51 +30,52 @@ class percona::repo {
         'percona-release-$basearch':
           descr   => 'Percona-Release YUM repository - $basearch',
           baseurl => 'http://repo.percona.com/release/$releasever/RPMS/$basearch',
-          enabled => 1;
+          enabled => $percona_release_basearch_enabled;
 
         'percona-release-noarch':
           descr   => 'Percona-Release YUM repository - noarch',
           baseurl => 'http://repo.percona.com/release/$releasever/RPMS/noarch',
-          enabled => 1;
+          enabled => $percona_release_noarch_enabled;
 
         'percona-release-source':
           descr   => 'Percona-Release YUM repository - Source packages',
           baseurl => 'http://repo.percona.com/release/$releasever/SRPMS',
-          enabled => 0;
+          enabled => $percona_release_source_enabled;
 
         'percona-testing-$basearch':
           descr   => 'Percona-Testing YUM repository - $basearch',
           baseurl => 'http://repo.percona.com/testing/$releasever/RPMS/$basearch',
-          enabled => 0;
+          enabled => $percona_testing_basearch_enabled;
 
         'percona-testing-noarch':
           descr   => 'Percona-Testing YUM repository - noarch',
           baseurl => 'http://repo.percona.com/testing/$releasever/RPMS/noarch',
-          enabled => 0;
+          enabled => $percona_testing_noarch_enabled;
 
         'percona-testing-source':
           descr   => 'Percona-Testing YUM repository - Source packages',
           baseurl => 'http://repo.percona.com/testing/$releasever/SRPMS',
-          enabled => 0;
+          enabled => $percona_testing_source_enabled;
 
         'percona-experimental-$basearch':
           descr   => 'Percona-Experimental YUM repository - $basearch',
           baseurl => 'http://repo.percona.com/experimental/$releasever/RPMS/$basearch',
-          enabled => 0;
+          enabled => $percona_experimental_basearch_enabled;
 
         'percona-experimental-noarch':
           descr   => 'Percona-Experimental YUM repository - noarch',
           baseurl => 'http://repo.percona.com/experimental/$releasever/RPMS/noarch',
-          enabled => 0;
+          enabled => $percona_experimental_noarch_enabled;
 
         'percona-experimental-source':
           descr   => 'Percona-Experimental YUM repository - Source packages',
           baseurl => 'http://repo.percona.com/experimental/$releasever/SRPMS',
-          enabled => 0;
+          enabled => $percona_experimental_source_enabled;
       }
     }
     'Debian': {
       apt::source { 'percona-release':
+        ensure   => $ensure,
         location => 'http://repo.percona.com/apt',
         repos    => 'main',
         key      => {
